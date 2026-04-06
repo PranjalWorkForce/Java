c003b2a6544d070d8e306798e1c6a096155f3793
Pranjal <pranjallolenglish@gmail.com>
2026-04-06 17:59:33 +0545

    Add files via upload

---
+++ n_country_flag.java
@@ -0,0 +1,68 @@
+public class n_country_flag {
+    static void square() {
+        System.out.println("\t\t\t\t\t                            .............***********..........._____   JAY NEPAL JAY NEPAL JAY NEPAL JAY NEPAL");
+        System.out.println("\t\t\t\t\t                          ==*************************************===....");
+        System.out.println("<\\ [o . o]    <\\ [o . o] \t\t       *********************************************************        [o . o] />     [o . o]  />      [o . o]  />   ");
+        System.out.println("  \\|===+||      \\|===+||\t\t    ***************************************************************     ||===+|/       ||===+|/        ||===+|/   ");
+        System.out.println("   |_|-|_|       |_|-|_|\t\t*********************************************************************** |_|-|_|        |_|-|_|         |_|-|_|            ");
+        System.out.println("\nJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t|| \t\t\t   || \tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t JAY NEPAL");
+        System.out.println("JAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t|| \t\t\t   || \tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t JAY NEPAL");
+        System.out.println("JAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t|| \t\t\t   || \tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\n");
+        System.out.println(" <\\ [o . o]     <\\ [o . o]     <\\ [o . o]     <\\ [o . o]   || \t\t\t   || [o . o] />     [o . o] />     [o . o] />      [o . o]  />   ");
+        System.out.println("   \\|===+||       \\|===+||       \\|===+||       \\|===+||   || \t\t\t   || ||===+|/_______||===+|/       ||===+|/        ||===+|/      ");
+        System.out.println("    |_|-|_|        |_|-|_|        |_|-|_|        |_|-|_|   || \t\t\t   || |_|-|_|        |_|-|_|        |_|-|_|         |_|-|_|         ");
+        System.out.println("\nJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t|| \t\t\t   || \tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\n");
+        System.out.println(" <\\ [o . o]     <\\ [o . o]     <\\ [o . o]     <\\ [o . o]   || \t\t\t   || [o . o] />     [o . o] />     [o . o] />      [o . o]  />   ");
+        System.out.println("   \\|===+||       \\|===+||       \\|===+||       \\|===+||   || \t\t\t   || ||===+|/_______||===+|/       ||===+|/        ||===+|/      ");
+        System.out.println("    |_|-|_|        |_|-|_|        |_|-|_|        |_|-|_|   || \t\t\t   || |_|-|_|        |_|-|_|        |_|-|_|         |_|-|_|         ");
+        System.out.println("\nJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t|| \t\t\t   || \tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\n");
+        System.out.println(" <\\ [o . o]     <\\ [o . o]     <\\ [o . o]     <\\ [o . o]   || \t\t\t   || [o . o] />     [o . o] />     [o . o] />      [o . o]  />   ");
+        System.out.println("   \\|===+||       \\|===+||       \\|===+||       \\|===+||   || \t\t\t   || ||===+|/_______||===+|/       ||===+|/        ||===+|/      ");
+        System.out.println("    |_|-|_|        |_|-|_|        |_|-|_|        |_|-|_|   || \t\t\t   || |_|-|_|        |_|-|_|        |_|-|_|         |_|-|_|         ");
+        System.out.println("\nJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t|| \t\t\t   || \tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t JAY NEPAL");
+                System.out.println("JAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t|| \t\t\t   || \tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t JAY NEPAL");
+        System.out.println("JAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t|| \t\t\t   || \tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\n"); System.out.println(" <\\ [o . o]     <\\ [o . o]     <\\ [o . o]     <\\ [o . o]   || \t\t\t   || [o . o] />     [o . o] />     [o . o] />      [o . o]  />   ");
+        System.out.println("   \\|===+||       \\|===+||       \\|===+||       \\|===+||   || \t\t\t   || ||===+|/_______||===+|/       ||===+|/        ||===+|/      ");
+        System.out.println("    |_|-|_|        |_|-|_|        |_|-|_|        |_|-|_|   || \t\t\t   || |_|-|_|        |_|-|_|        |_|-|_|         |_|-|_|         ");
+        System.out.println("\nJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t|| \t\t\t   || \tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\n");
+        System.out.println(" <\\ [o . o]     <\\ [o . o]     <\\ [o . o]     <\\ [o . o]   || \t\t\t   || [o . o] />     [o . o] />     [o . o] />      [o . o]  />   ");
+        System.out.println("   \\|===+||       \\|===+||       \\|===+||       \\|===+||   || \t\t\t   || ||===+|/_______||===+|/       ||===+|/        ||===+|/      ");
+        System.out.println("    |_|-|_|        |_|-|_|        |_|-|_|        |_|-|_|   || \t\t\t   || |_|-|_|        |_|-|_|        |_|-|_|         |_|-|_|         ");
+        System.out.println("\nJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t|| \t\t\t   || \tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\n");
+        System.out.println(" <\\ [o . o]     <\\ [o . o]     <\\ [o . o]     <\\ [o . o]   || \t\t\t   || [o . o] />     [o . o] />     [o . o] />      [o . o]  />   ");
+        System.out.println("   \\|===+||       \\|===+||       \\|===+||       \\|===+||   || \t\t\t   || ||===+|/_______||===+|/       ||===+|/        ||===+|/      ");
+        System.out.println("    |_|-|_|        |_|-|_|        |_|-|_|        |_|-|_|   || \t\t\t   || |_|-|_|        |_|-|_|        |_|-|_|         |_|-|_|         ");
+        System.out.println("\nJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t|| \t\t\t   || \tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t JAY NEPAL");
+                System.out.println("JAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t|| \t\t\t   || \tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t JAY NEPAL");
+        System.out.println("JAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t|| \t\t\t   || \tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\tJAY NEPAL\t JAY NEPAL\t JAY NEPAL\n");
+
+
+    }
+
+
+    static void rightAngleTriangle() {
+        for (int i = 1; i <= 7; i++) {
+            System.out.print("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    static void rod() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t");
+            System.out.print(" * \n");
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        rightAngleTriangle();
+        rightAngleTriangle();
+        rod();
+        square();
+    }
+}
